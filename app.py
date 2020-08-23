889ec92bfd89c9e529e24977d4f5cb6c00021ff1
Sopiro <knwer782@naver.com>
2020-08-23 12:02:05 +0900

    Image upload, delete

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 import os
 from werkzeug.utils import secure_filename
+import shutil
 
 app = Flask(__name__)
 app.debug = True
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
 app.secret_key = 'sopiro'
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -24,14 +31,33 @@ def file_upload():
         if f.filename == '':
             return redirect(request.url)
 
-        filename = secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-        return render_template('result.html', filename=filename)
+            return render_template('result.html', filename=filename)
+        else:
+            return redirect(request.url)
+
 
 @app.route('/display/<filename>')
 def display_image(filename):
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+
+@app.route('/delete/<filename>')
+def delete_file(filename):
+    try:
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    except FileNotFoundError:
+        pass
+    return redirect(url_for('index'))
+
+# @app.teardown_request
+# def teardown_request(ex):
+#     for f in os.listdir(app.config['UPLOAD_FOLDER']):
+#         os.remove(f)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
